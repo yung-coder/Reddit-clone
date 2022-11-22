b996550e89fbc0df4854467a4ebc3d245dfeb05d
yung-coder <chandel.sanjeev5@gmail.com>
2022-11-23 00:36:11 +0530

    init

--- components/Layout/PageContent.tsx
+++ components/Layout/PageContent.tsx
@@ -2,7 +2,7 @@ import { Flex } from "@chakra-ui/react";
 import React from "react";
 
 type PageContentProps = {
-  children: React.PropsWithChildren;
+  children: any;
 };
 
 const PageContent: React.FC<PageContentProps> = ({ children }) => {
